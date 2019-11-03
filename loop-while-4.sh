4fd2c38f8e87fab6f25fb5f2e40ab611b06d0a77
Yerlin Matu <maicolmatu27@gmail.com>
2019-11-02 22:46:30 -0500

    Removed tabs

--- loop-while-4.sh
+++ loop-while-4.sh
@@ -3,14 +3,14 @@ item=1;
 
 while [[ $item -le 20 ]]; do
   case $item in 
-		4) echo $item $'\nbad luck from China'
-		;;
-		9) echo $item $'\nbad luck from Japan'
-		;;
-		17) echo $item $'\nbad luck from Italy'
-		;;
-		*) echo $item
-		;;
-	esac
+	  4) echo $item $'\nbad luck from China'
+	  ;;
+	  9) echo $item $'\nbad luck from Japan'
+	  ;;
+	  17) echo $item $'\nbad luck from Italy'
+	  ;;
+	  *) echo $item
+	  ;;
+  esac
 	let item+=1
 done
