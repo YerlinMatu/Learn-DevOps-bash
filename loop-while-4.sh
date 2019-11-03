aab14cd0e873d0684c02811fe19450b7ab6cf909
Yerlin Matu <maicolmatu27@gmail.com>
2019-11-02 23:05:17 -0500

    Removed variable item in case

--- loop-while-4.sh
+++ loop-while-4.sh
@@ -2,15 +2,14 @@ clear
 item=1;
 
 while [[ $item -le 20 ]]; do
-  case $item in 
-	  4) echo $item $'\nbad luck from China'
-	  ;;
-	  9) echo $item $'\nbad luck from Japan'
-	  ;;
-	  17) echo $item $'\nbad luck from Italy'
-	  ;;
-	  *) echo $item
-	  ;;
+    echo $item
+    case $item in
+      4) echo "bad luck from China"
+      ;;
+      9) echo "bad luck from Japan"
+      ;;
+      17) echo "bad luck from Italy"
+      ;;
   esac
 	let item+=1
 done
