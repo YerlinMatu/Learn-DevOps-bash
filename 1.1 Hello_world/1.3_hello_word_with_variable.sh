06f5e6f026e64bf1142098e274d817572ec32962
Yerlin Matu <yerlinsonmaturana@seven4n.com>
2019-11-17 13:47:23 -0500

    Added hello_world chapter 1

---
+++ 1.1 Hello_world/1.3_hello_word_with_variable.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+whom_variable="World"
+
+printf "Hello %s\n" "$whom_variable"
---
+++ 1.1 Hello_world/1.4 hello_world_with_user_input.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo "Who are you?"
+read name
+echo "Hello, $name"
+
+# OR
+echo "What do you doing"
+read action
+echo "You are ${action}ing"
+
